995fcbeb0183ed1dd5bcaa36ca24afd25a59a1da
Tahsin Hasan <tahsin.hasan@metafour.com>
2023-10-21 13:38:05 +0600

    Updated weather api token

--- lib/services/weather_api_client.dart
+++ lib/services/weather_api_client.dart
@@ -2,19 +2,20 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:weather/model/weather.dart';
+import 'package:weather/util/constant.dart';
 
 
 class WeatherApiClient{
 
-  Future<Weather>? getCurrentWeatherByLatLong(String? lot, String? long) async{
-      var endPoint= Uri.parse("https://api.openweathermap.org/data/2.5/weather?lat=37.4219983&lon=-122.084&appid=eaf0fa7acda09fcd8d7f8179161f6e72");
+  Future<Weather>? getCurrentWeatherByLatLong(String? lat, String? long) async{
+      var endPoint= Uri.parse("https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$long&appid=$apiToken");
       var response = await http.get(endPoint);
       var body = jsonDecode(response.body);
       return Weather.fromJson(body);  
   }
 
    Future<Weather>? getCurrentWeatherLocation(String? location) async{
-      var endPoint = Uri.parse("https://api.openweathermap.org/data/2.5/weather?q=$location&APPID=eaf0fa7acda09fcd8d7f8179161f6e72");
+      var endPoint = Uri.parse("https://api.openweathermap.org/data/2.5/weather?q=$location&APPID=$apiToken");
       var response = await http.get(endPoint);
       var body = jsonDecode(response.body);
       return Weather.fromJson(body);  
--- lib/util/constant.dart
+++ lib/util/constant.dart
@@ -2,3 +2,4 @@
 
 const String defaultLat= "23.7104";
 const String defaultLong= "90.4074";
+const String apiToken= "eaf0fa7acda09fcd8d7f8179161f6e72";
